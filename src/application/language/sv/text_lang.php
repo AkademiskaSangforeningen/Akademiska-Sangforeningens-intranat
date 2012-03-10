9184289ff0a9a15b17b02be7011c214d55fc666c
Andre Brunnsberg <andre.brunnsberg@gmail.com>
2012-03-10 20:35:50 +0200

    * Language file for Swedish interface

---
+++ src/application/language/sv/text_lang.php
@@ -0,0 +1,16 @@
+﻿<?php  
+/**
+ * Swedish language file
+ *
+ * @author André Brunnsberg
+ *
+*/
+
+$lang['login_header']					= "Akademiska sångföreningens intranät";
+
+$lang['login_form_submit']				= "Logga in dig";
+
+$lang['field_email']					= "E-postaddress";
+$lang['field_password']					= "Lösenord";
+
+$lang['error_wrong_credentials']		= "Fel användarnamn eller lösenord";
