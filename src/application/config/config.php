8cc427aa9bbaf69960669216629969637843f9dc
Andre Brunnsberg <andre.brunnsberg@gmail.com>
2012-03-10 20:33:34 +0200

    * Added an encryption key uses for session encryption and password hashing (using bcrypt) *

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'R?+gR$A6.r3[Xw|!g8_l,<J(#T#!NW22';
 
 /*
 |--------------------------------------------------------------------------
