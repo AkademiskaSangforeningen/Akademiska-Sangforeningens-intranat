a6055fd0a48b60f66388fda0c296b9f7ed7ca331
efloman <emil.floman@gmail.com>
2012-07-12 17:34:57 +0300

    Added events tab

--- src/application/views/desktop/generic/header_navitabs.php
+++ src/application/views/desktop/generic/header_navitabs.php
@@ -1,7 +1,7 @@
 <div id="header_navitabs" style="clear: both; display: none">
 	<ul>		
 		<li><a href="#tab_dashboard">Min sida</a></li>
-		<li><a href="#tabs-2">Evenemang</a></li>
+		<li><a href="<?php echo CONTROLLER_EVENTSIGNUP_LISTALL ?>">Evenemang</a></li>
 		<li><a href="<?php echo CONTROLLER_TRANSACTIONS_LISTALL ?>">Kvartettkonto</a></li>
 		<li><a href="<?php echo CONTROLLER_PERSONS_LISTALL ?>">Medlemmar</a></li>
 	</ul>
