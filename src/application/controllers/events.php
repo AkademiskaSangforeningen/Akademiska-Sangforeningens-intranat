37af9b6633d06eaa8c44570277604a2304e1d9bb
Andre Brunnsberg <andre.brunnsberg@gmail.com>
2012-12-09 22:07:08 +0100

    Changed shown fields for internal registration

--- src/application/controllers/events.php
+++ src/application/controllers/events.php
@@ -178,7 +178,7 @@ class Events extends CI_Controller {
 		$data_part_form_person = array();
 		$data_part_form_person['person']		= $this->person->getPerson($personId);
 		$data_part_form_person['fieldPrefix']	= '';
-		$data_part_form_person['showFields']	= ($personId != NULL) ? array(DB_PERSON_ALLERGIES) 
+		$data_part_form_person['showFields']	= ($personId != NULL) ? array(DB_PERSON_FIRSTNAME, DB_PERSON_LASTNAME, DB_PERSON_PHONE, DB_PERSON_ALLERGIES) 
 														: array(DB_PERSON_FIRSTNAME, DB_PERSON_LASTNAME, DB_PERSON_EMAIL, DB_PERSON_PHONE, DB_PERSON_ALLERGIES);
 		$data['part_form_person'] = $this->load->view($client . VIEW_CONTENT_EVENTS_PART_FORM_PERSON, $data_part_form_person, TRUE);
 			
