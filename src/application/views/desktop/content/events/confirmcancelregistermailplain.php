811bb737536ca8ffc269a8e6457e11884ef628d2
Andre Brunnsberg <andre.brunnsberg@gmail.com>
2012-12-16 21:33:05 +0100

    Rewrote plain message for cancel registration email

--- src/application/views/desktop/content/events/confirmcancelregistermailplain.php
+++ src/application/views/desktop/content/events/confirmcancelregistermailplain.php
@@ -1,6 +1,7 @@
-<?php echo $header; ?>
-<?php echo "\r\n"; ?>
-<?php echo "\r\n"; ?>
-Klicka på länken nedan för att anmäla dig på nytt:
-<?php echo "\r\n"; ?>
-<?php echo site_url() . CONTROLLER_EVENTS_EDIT_REGISTER_DIRECTLY; ?>/<?php echo $eventId; ?>
+<?php
+echo $header;
+echo "\r\n";
+echo "\r\n";
+echo 'Klicka på länken nedan för att anmäla dig på nytt:';
+echo "\r\n";
+echo site_url() . CONTROLLER_EVENTS_EDIT_REGISTER_DIRECTLY . '/' . $eventId;
