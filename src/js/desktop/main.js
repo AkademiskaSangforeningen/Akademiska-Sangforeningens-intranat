c23e497b895c6baf1a1befd2dac55da9d6ebf99b
Andre Brunnsberg <andre.brunnsberg@gmail.com>
2012-12-29 17:35:03 +0200

    Fixed JS-IE bugs, checked code goes through JSHINT

--- src/js/desktop/main.js
+++ src/js/desktop/main.js
@@ -1,13 +1,14 @@
 var AKADEMEN = (function(){
-  
+	"use strict";
+
     //Return public methods
-	return {
-	
+	return {		
+
 		/**
 		*	Initialize dialog for forms
-		*/		
-		initializeFormDialog: function(langSave, langCancel) {					
-			$('<div id="dialog_form"></div>')						
+		*/
+		initializeFormDialog: function(langSave, langCancel) {
+			$('<div id="dialog_form"></div>')
 				.dialog({
 					autoOpen: false,
 					height: ($(window).height() * 0.9),
@@ -18,14 +19,14 @@ var AKADEMEN = (function(){
 							text: langSave,
 							click: function () {
 								$('#form_editobject').trigger('submit');
-							},
+							}
 						},
 						{
 							text: langCancel,
 							"data-priority": "secondary",
 							click: function () {
 								$( this ).dialog( "close" );
-							},
+							}
 						}
 					],
 					open: function() {
@@ -35,15 +36,15 @@ var AKADEMEN = (function(){
 								.find('.ui-dialog-buttonpane button[data-priority]')
 									.each(function() {
 										$(this).addClass("ui-priority-" + $(this).attr("data-priority"));
-									});			
-						
+									});
+
 						}
 				});
 		},
-		
+
 		/**
 		*	Initialize dialog for confirmation
-		*/		
+		*/
 		initializeConfirmDialog: function(langConfirm, langOK, langCancel) {
 			$('<div id="dialog_confirm"></div>')
 				.html("<p>" + langConfirm + "</p>")
@@ -57,7 +58,7 @@ var AKADEMEN = (function(){
 							"data-priority": "secondary"
 						},
 						{
-							text: langCancel,							
+							text: langCancel,
 							click: function () {
 								$( this ).dialog( "close" );
 							}
@@ -70,15 +71,15 @@ var AKADEMEN = (function(){
 								.find('.ui-dialog-buttonpane button[data-priority]')
 									.each(function() {
 										$(this).addClass("ui-priority-" + $(this).attr("data-priority"));
-									});			
-						
+									});
+
 						}
 				});
 		},
-		
+
 		/**
 		*	Initialize dialog for alert
-		*/		
+		*/
 		initializeAlertDialog: function(langOK) {
 			$('<div id="dialog_alert"></div>')
 				.dialog({
@@ -98,25 +99,25 @@ var AKADEMEN = (function(){
 
 		/**
 		*	Initialize dialog for confirmation
-		*/		
+		*/
 		initializeListDialog: function() {
-			$('<div id="dialog_list"></div>')						
+			$('<div id="dialog_list"></div>')
 				.dialog({
 					autoOpen: false,
 					height: ($(window).height() * 0.9),
 					width: ($(window).width() * 0.9),
 					modal: true
 				});
-		},	
-	
+		},
+
 		/**
 		*	Initialize buttons
 		*	@param	queryFilter	Filter that can be used for filtering which buttons to initialize
-		*/		
-		initializeButtons: function(queryFilter) {	
+		*/
+		initializeButtons: function(queryFilter) {
 			//If parameter is undefined, replace it with an empty link
 			queryFilter = (queryFilter) || "";
-		
+
 			//Initialize not already initialized buttons
 			$(queryFilter + " .button:not(.ui-button)")
 				.each(function() {
@@ -124,45 +125,45 @@ var AKADEMEN = (function(){
 						text = $(this).data("text");
 					$(this)
 						.button({ icons: { primary: icon }, text: text })
-						.css("display", "inline-block");			
+						.css("display", "inline-block");
 				})
 				//Special handling for those buttons that open a form dialog
 				.filter('[data-formdialog="true"]')
-					.on('click.openFormDialog', function (event) {			
+					.on('click.openFormDialog', function (event) {
 						var title = $(this).text();
 						$.ajax({
-							url: $(this).attr("href"),				  
+							url: $(this).attr("href"),
 							dataType: "html",
 							cache: false,
 							//on success, set the data to the dialog and open it
 							success: function(data) {
 								$('#dialog_form')
 									.html(data)
-									.dialog("option", "title", title)								
-									.dialog('open');							
+									.dialog("option", "title", title)
+									.dialog('open');
 							},
 							error: function(jqXHR, textStatus, errorThrown) {
-								alert(errorThrown);
+								window.alert(errorThrown);
 							}
-						});											
-						return false;			
+						});
+						return false;
 					})
 					.end()
 				//Special handling for those buttons that open a confirmation dialog
 				.filter('[data-confirmdialog="true"]')
-					.on('click.openConfirmDialog', function (event) {													
+					.on('click.openConfirmDialog', function (event) {
 						var $this = $(this),
-							title = $this.text();	
-							
+							title = $this.text();
+
 						$('#dialog_confirm')
-							.dialog("option", "title", title)								
-							.dialog('open')						
+							.dialog("option", "title", title)
+							.dialog('open')
 							.closest('.ui-dialog')
 								.find('button:first')
-									.off('click')							
+									.off('click')
 									.on('click.confirmConfirmDialog', function (event) {
 										$.ajax({
-											url: $this.attr("href"),				  
+											url: $this.attr("href"),
 											dataType: "html",
 											cache: false,
 											//on success, set the data to the dialog and open it
@@ -171,23 +172,23 @@ var AKADEMEN = (function(){
 													.html(data);
 											},
 											error: function(jqXHR, textStatus, errorThrown) {
-												alert(errorThrown);
+												window.alert(errorThrown);
 											}
-										});	
-										
+										});
+
 										return false;
-									});						
-						return false;			
+									});
+						return false;
 					})
 					.end()
 				//Special handling for those buttons that open a list dialog
 				.filter('[data-listdialog="true"]')
-					.on('click.openListDialog', function (event) {			
+					.on('click.openListDialog', function (event) {
 						var title = $(this).text(),
 							url = $(this).attr("href");
-							
+
 						$.ajax({
-							url: url,				  
+							url: url,
 							dataType: "html",
 							cache: false,
 							//on success, set the data to the dialog and open it
@@ -195,43 +196,42 @@ var AKADEMEN = (function(){
 								$('#dialog_list')
 									.data("url", url)
 									.html(data)
-									.dialog("option", "title", title)								
+									.dialog("option", "title", title)
 									.dialog('open');
-								AKADEMEN.initializeButtons(); 									
+								AKADEMEN.initializeButtons();
 							},
 							error: function(jqXHR, textStatus, errorThrown) {
-								alert(errorThrown);
+								window.alert(errorThrown);
 							}
-						});											
-						return false;			
-					})
-					.end()					
+						});
+						return false;
+					});
 		},
-		
+
 		/**
 		*	Initialize tabs
 		*/
 		initializeTabs: function() {
 			$('#header_navitabs')
-				.tabs({ 
+				.tabs({
 					load: function() {
-						AKADEMEN.initializeButtons(); 
+						AKADEMEN.initializeButtons();
 					}
 				})
 				.show();
 		},
-		
+
 		/**
 		*	Initialize form validation.
 		*	Validation is automatically done when the form is submitted.
 		*/
-		initializeFormValidation: function(defaultPost) {		
+		initializeFormValidation: function(defaultPost) {
 			$('#form_editobject')
 				.validate({
 					submitHandler: function(form) {
 						if (defaultPost) {
 							form.submit();
-						} else {					
+						} else {
 							$.ajax({
 								type: 'POST',
 								url: $(form).attr("action"),
@@ -240,33 +240,33 @@ var AKADEMEN = (function(){
 									$('#dialog_form').html(data);
 								},
 								error:  function(jqXHR, textStatus, errorThrown) {
-									alert(errorThrown);
+									window.alert(errorThrown);
 								},
 								dataType: "html"
 							});
 						}
-						
+
 						return false;
 					}
 				});
 		},
-		
+
 		/**
 		*	Initialize date picker default values.
 		*/
-		initializeDatepicker: function() {			
+		initializeDatepicker: function() {
 			$.datepicker.setDefaults({
 						changeMonth: true,
 						changeYear: true,
 						showWeek: true,
 						firstDay: 1,
-						dateFormat: 'dd.mm.yy'			
+						dateFormat: 'dd.mm.yy'
 			});
 		},
 
 		/**
 		*	Initialize overlay shown when ajax-calls are being made
-		*/		
+		*/
 		initializeAjaxLoading: function() {
 			$('#ajaxloading')
 				.ajaxStart(function() {
