dfca23e47f105c3f36d638cdcad3b016278e35c1
Andre Brunnsberg <andre.brunnsberg@gmail.com>
2013-01-14 00:30:05 +0200

    Only list internally events that "active singers" are set as participiants

--- src/application/models/event.php
+++ src/application/models/event.php
@@ -239,6 +239,7 @@ class Event extends CI_Model {
 		$this->db->select('(SELECT COUNT(*) FROM ' . DB_TABLE_PERSONHASEVENT . ' AS A WHERE A.' . DB_PERSONHASEVENT_EVENTID . ' = ' . DB_TABLE_EVENT . '.' . DB_EVENT_ID . ') AS ' . DB_TOTALCOUNT, FALSE);
 		$this->db->from(DB_TABLE_EVENT);		
 		$this->db->where('NOT EXISTS (SELECT 1 FROM ' . DB_TABLE_PERSONHASEVENT . ' WHERE ' . DB_TABLE_EVENT . '.' . DB_EVENT_ID . ' = ' . DB_TABLE_PERSONHASEVENT . '.' . DB_PERSONHASEVENT_EVENTID . ' AND ' .  DB_TABLE_PERSONHASEVENT . '.' . DB_PERSONHASEVENT_PERSONID . ' = \'' . $personId . '\')', NULL, FALSE);
+		$this->db->where(DB_EVENT_PARTICIPANT . ' & 1', NULL, FALSE); // Only list events that "active singers" are set as participiants
 		$this->db->order_by(DB_EVENT_STARTDATE);
 		$this->db->order_by(DB_EVENT_ENDDATE);
 		$this->db->order_by(DB_EVENT_NAME);			
